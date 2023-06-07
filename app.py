43a0c8242eacd097ac5c0b0b6af7f70b4ab54de9
Indra Prayoga S <yogadartmedia@gmail.com>
2023-06-07 15:33:28 +0700

    ubah port

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ async def create_upload_file(file1: UploadFile = File(...),file2: UploadFile = F
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9001)
